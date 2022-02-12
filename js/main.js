c0cb862b242165d0f5896d4e8e23d3f9e2142686
Juan_Esteban_Cardona <juanes.cardona@hotmail.com>
2022-02-12 18:33:08 -0500

    Segundo commit funcional, se crea la funcion bar

--- js/main.js
+++ js/main.js
@@ -10,7 +10,8 @@
     self.Board.prototype = {
         get elements(){
             var elements = this.bars;
-            elements.push(ball);
+            elements.push(this.ball);
+            return elements;
 
         }
     }
@@ -28,15 +29,59 @@
         this.ctx = canvas.getContext('2d');
     }
 
+    self.BoardView.prototype = {
+        draw: function(){
+            for ( var i = this.board.elements.length - 1; i >= 0; i--) {
+                var el = this.board.elements[i]
+
+                draw(this.ctx,el)
+            };
+        }
+    }
+
+    function draw(ctx,element){
+        if(element != null && element.hasOwnProperty("kind")){
+            switch(element.kind){
+                case "rectangle":
+                    ctx.fillRect(element.x,element.y,element.width,element.height);
+                    break;
+                }
+        }
     console.log("BoardView function loaded");
+    }
+
 })();
-        
+ 
+
+(function(){
+    self.Bar = function(x,y,width,height,board){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.board = board;
+        this.board.bars.push(this);
+        this.kind = "rectangle";
+    }
+    self.Bar.prototype ={
+        down: function(){
+        },
+        up: function(){
+        }
+
+    }
+    console.log("Bar function loaded");
+})();
 window.addEventListener("load",main);
 
 function main(){
     var board = new Board(800,400);
+    var bar = new Bar(20,100,20,100,board);
+    var bar = new Bar(760,100,20,100,board);
     var canvas = document.getElementById("canvas");
     var board_view = new BoardView(canvas,board);
+
+    board_view.draw();
 
     console.log("main function loaded");
 
